3199345b4c6334cce4ef532d34899af6202b832d
Thuan H. Nguyen <32274287+thuanz123@users.noreply.github.com>
2023-06-03 12:54:43 +0700

    Update training_loop.py

--- training/training_loop.py
+++ training/training_loop.py
@@ -33,33 +33,10 @@ def setup_snapshot_image_grid(training_set, random_seed=0):
     gw = np.clip(7680 // training_set.image_shape[2], 7, 32)
     gh = np.clip(4320 // training_set.image_shape[1], 4, 32)
 
-    # No labels => show random subset of training samples.
-    if not training_set.has_labels:
-        all_indices = list(range(len(training_set)))
-        rnd.shuffle(all_indices)
-        grid_indices = [all_indices[i % len(all_indices)] for i in range(gw * gh)]
-
-    else:
-        # Group training samples by label.
-        label_groups = dict() # label => [idx, ...]
-        for idx in range(len(training_set)):
-            label = tuple(training_set.get_details(idx).raw_label.flat[::-1])
-            if label not in label_groups:
-                label_groups[label] = []
-            label_groups[label].append(idx)
-
-        # Reorder.
-        label_order = sorted(label_groups.keys())
-        for label in label_order:
-            rnd.shuffle(label_groups[label])
-
-        # Organize into grid.
-        grid_indices = []
-        for y in range(gh):
-            label = label_order[y % len(label_order)]
-            indices = label_groups[label]
-            grid_indices += [indices[x % len(indices)] for x in range(gw)]
-            label_groups[label] = [indices[(i + gw) % len(indices)] for i in range(len(indices))]
+    # Show random subset of training samples.
+    all_indices = list(range(len(training_set)))
+    rnd.shuffle(all_indices)
+    grid_indices = [all_indices[i % len(all_indices)] for i in range(gw * gh)]
 
     # Load data.
     images, labels = zip(*[training_set[i] for i in grid_indices])
